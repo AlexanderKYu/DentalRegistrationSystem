48d4878c839e73b0d64c86cc1f5036d62b57730a
Alexis Verana <avera085@uottawa.ca>
2022-04-12 21:29:41 -0400

    Committing files to branch

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
 from db import *
 from patientsearchform import PatientSearchForm
 import sqlite3
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '6f80377f374a443dd2288eb00e322026'
+
+
 
 @app.route("/")
 def home():
@@ -18,9 +22,22 @@ def user_info():
     print(get_users())
     return "Users page has rendered!"
 
-@app.route("/patient")
+@app.route("/patient", methods=['GET', 'POST'])
 def patient():
     form = PatientSearchForm()
+
+    if request.method == "POST":
+       first_name = request.form.get("fname")
+
+       last_name = request.form.get("lname") 
+
+       dob = request.form.get("dob")
+
+       patientID = get_pat_fName_LName_DOB(first_name, last_name, dob)[0][0]
+       
+       return "record " + str (get_record_patient_ID(patientID))
+
+
     return render_template('patient.html', title ='Patient', form=form)
 
 @app.route("/emp")
